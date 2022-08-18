94401b0ece66b178fc3a73ad51edac439f8c5252
emilyz22 <110996979+emilyz22@users.noreply.github.com>
2022-08-18 16:46:24 -0400

    Final Project 8/18

--- Final Project/FinalProject/FinalProject/ViewController.swift
+++ Final Project/FinalProject/FinalProject/ViewController.swift
@@ -5,10 +5,14 @@
 //  Created by Scholar on 8/16/22.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var buttonDown1: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
